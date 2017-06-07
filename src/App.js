8995a5fc9c7520425c878a31544c525faec110a3
marleysidapple <bsiddhartha25@gmail.com>
2017-06-07 23:43:39 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-//text is not necessary in here. 
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';

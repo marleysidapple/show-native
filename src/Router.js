14dbd0d82c36d04b621c5a6caa92cfd6ee415482
marleysidapple <bsiddhartha25@gmail.com>
2017-06-09 09:17:34 +0800

    added employee scene

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@
 import React, { Component } from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import Loginform from './components/Loginform';
+import Employeelist from './components/Employeelist';   //second screen
 
 
 class RouterComponent extends Component {
@@ -14,9 +15,11 @@ class RouterComponent extends Component {
 
 	render(){
 		//passing style to scene using SceneStyle : global styling and gets applied to all the scenes
+		//key could be anything
 		return(
 			<Router sceneStyle={{paddingTop: 65}}>
 	 			<Scene key="login" component={Loginform} title={'Please Login'} />
+	 			<Scene key="employeelist" component={Employeelist} title={'Employees'} />
 	 		</Router>
 		);
 	}
---
+++ src/components/Employeelist.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+class Employeelist extends Component {
+	
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<View>
+				<Text>Employee List</Text>
+				<Text>Employee List</Text>
+				<Text>Employee List</Text>
+				<Text>Employee List</Text>
+				<Text>Employee List</Text>
+			</View>
+		);
+	}
+}
+
+export default Employeelist;
